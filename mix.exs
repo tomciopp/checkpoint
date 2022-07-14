68ec29d9ff51e601782afe0901e06d26f282fe22
Thomas Cioppettini <544875+tomciopp@users.noreply.github.com>
2022-07-14 05:00:46 -0400

    chore(Deps): Update deps and bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Checkpoint.MixProject do
   def project do
     [
       app: :checkpoint,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
